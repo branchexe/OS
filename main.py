a0629ab51101e4587064a3c8337a7597bbde2a0c
branchexe <64759630+branchexe@users.noreply.github.com>
2020-06-20 21:15:32 -0500

    added right click

--- main.py
+++ main.py
@@ -8,22 +8,35 @@ def displayTime():
     timeLabel.config(text=currentTime + "  " + currentDate)
     root.after(200,displayTime)
 
+
+
 #       ~~~~~~~~~~~~~~~setup stuff~~~~~~~~~~~~~~~
 root = Tk(); #create window
-
 #create window size
 root.geometry("512x512")
-
 #name window
 root.title("NathOS");
-
 #set bgcolor
 root.configure(bg="yellow")
 
 
+
+#Right clicks on desktop
 def RightClickPos(event):
+    def RightWindowDestroy(event):
+        RightWindow.destroy()
     Desktop.focus_set();
-    print("Clicked at" + str(event.x) + str(event.y))
+    #print("Clicked at" + str(event.x) + str(event.y))
+    RightWindow = Toplevel(Desktop) #create new window
+    RightWindowButton1 = Button(RightWindow, text="Color options").grid(row=0)
+    RightWindowButton2 = Button(RightWindow, text="Other options").grid(row=1)
+    
+    RightWindow.overrideredirect(1) #get rid of min/max buttons
+    #set position
+    x = root.winfo_x()
+    y = root.winfo_y()
+    RightWindow.geometry("+%d+%d" % (x + event.x, y + event.y))
+    Desktop.bind("<Button-1>", RightWindowDestroy) #Destory TopLevel
 
 
 #create desktop frame
